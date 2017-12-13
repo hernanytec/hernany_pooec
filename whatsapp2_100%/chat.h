f14fa7aa22d5dcbdad561f67d2b9532359935cb5
Hernany <hernnaytec777@gmail.com>
2017-12-13 09:27:54 -0300

    second commit

---
+++ whatsapp2_100%/chat.h
@@ -0,0 +1,11 @@
+#ifndef CHAT_H
+#define CHAT_H
+
+#include "_zap.h"
+
+
+using namespace std;
+
+
+
+#endif // CHAT_H
---
+++ whatsapp2_100%/user.h
@@ -0,0 +1,7 @@
+#ifndef USER_H
+#define USER_H
+
+using namespace std;
+
+
+#endif // USER_H
